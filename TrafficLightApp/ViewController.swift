38406bd053357f6e1116739d16d877f64bb96ba2
ZasyadkoAA <aleks.zasyadko@icloud.com>
2022-08-15 14:57:22 +0300

    hw 2

--- TrafficLightApp/ViewController.swift
+++ TrafficLightApp/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var switchButton: UIButton!
+    
+    var lightOn: CGFloat = 1
+    var lightOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redView.layer.cornerRadius = 60
+        yellowView.layer.cornerRadius = 60
+        greenView.layer.cornerRadius = 60
+        switchButton.layer.cornerRadius = 10
+        yellowView.alpha = lightOff
+        greenView.alpha = lightOff
+        
     }
 
+    @IBAction func switchButtonPress() {
+        if switchButton.currentTitle == "START" {
+            switchButton.setTitle("NEXT", for: .normal)
+        }
+        if redView.alpha == 1 {
+            redView.alpha = lightOff
+            yellowView.alpha = lightOn
+        } else if yellowView.alpha == 1 {
+            yellowView.alpha = lightOff
+            greenView.alpha = lightOn
+        } else if greenView.alpha == 1 {
+            greenView.alpha = lightOff
+        } else {
+            redView.alpha = lightOn
+        }
+            
+        }
 
 }
 
